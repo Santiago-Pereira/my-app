5a0abd24cd663ce6b11242c200993ce5309f41b5
Santiago-Pereira <santiagopereira_8@hotmail.com>
2023-01-17 21:20:03 -0300

    "movie app"

---
+++ src/movieCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+//movie card component
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={movie.Title}
+        />
+      </div>
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+        {/*  <button
+          onClick={() => {
+            console.log("ive been clicked");
+          }}
+        >
+          Full description
+        </button> */}
+      </div>
+    </div>
+  );
+};
+export default MovieCard;
